b04d9f30b13f5dae5853c4593ab9cbf8cd731e71
Bhargav <vyasb1503@gmail.com>
2025-06-18 23:47:13 +0530

    implementation of the user entity

--- src/main/java/com/tka/Entity/UserEntity.java
+++ src/main/java/com/tka/Entity/UserEntity.java
@@ -1,8 +1,16 @@
 package com.tka.Entity;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class UserEntity {
-
+	@Id
+	private int id;
+	private String name;
+	private String email;
+	private String password;
+	private List<String> skills; 
 }
