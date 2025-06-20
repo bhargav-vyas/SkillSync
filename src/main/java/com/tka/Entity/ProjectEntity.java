1eb4cb862dc6437258cbea6f63ec5370a148165c
Bhargav <vyasb1503@gmail.com>
2025-06-20 18:25:07 +0530

    this is the  adding  of the project entity

--- src/main/java/com/tka/Entity/ProjectEntity.java
+++ src/main/java/com/tka/Entity/ProjectEntity.java
@@ -1,6 +1,21 @@
 package com.tka.Entity;
 
-public class ProjectEntity {
+import java.security.PrivateKey;
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class ProjectEntity {
+	@Id
+ private int id;
+ private String title;
+ private List<String> requiredSkill;
+ private String PostedBy;
+ private int teamSize;
+ private  String deadline;
+ 
+ 
 	
 }
