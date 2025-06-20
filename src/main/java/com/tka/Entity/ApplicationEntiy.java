d524147db9ab1735b7b42920c0f4616b76dadf62
Bhargav <vyasb1503@gmail.com>
2025-06-20 19:43:43 +0530

    this is the adding the one entity

--- src/main/java/com/tka/Entity/ApplicationEntiy.java
+++ src/main/java/com/tka/Entity/ApplicationEntiy.java
@@ -2,10 +2,11 @@ package com.tka.Entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class ApplicationEntiy {
 @Id
 private  int id;
-
+private  int userid;
 }
