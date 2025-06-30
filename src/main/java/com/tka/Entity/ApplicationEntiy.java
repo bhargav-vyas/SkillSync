408fd09e8e329ed93a21f0a7a0f8251cb81bb3b0
Bhargav <vyasb1503@gmail.com>
2025-06-30 18:06:43 +0530

    changes in the application entity

--- src/main/java/com/tka/Entity/ApplicationEntiy.java
+++ src/main/java/com/tka/Entity/ApplicationEntiy.java
@@ -1,14 +1,19 @@
 package com.tka.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 
 @Entity
 public class ApplicationEntiy {
 @Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
 private  int id;
-private  int userid;
-private  int projectid;
+@ManyToOne
+private  UserEntity user;
+@ManyToOne
+private  ProjectEntity project;
 
 }
