f1164487217bd81ffcc07f45cfb1dcef7702d67e
Bhargav <vyasb1503@gmail.com>
2025-07-02 00:40:56 +0530

    implementation of the  security configuration

--- src/main/java/com/tka/Controller/UserController.java
+++ src/main/java/com/tka/Controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tka.Entity.UserEntity;
-import com.tkaService.UserService;
+import com.tka.Service.UserService;
 
 @Controller
 @RestController
