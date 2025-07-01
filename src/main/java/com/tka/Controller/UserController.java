c0746fe7e17b1d0e1a094936dc57d65d264267c1
Bhargav <vyasb1503@gmail.com>
2025-07-02 00:33:12 +0530

    implementation of the  security configuration

--- src/main/java/com/tka/Controller/UserController.java
+++ src/main/java/com/tka/Controller/UserController.java
@@ -1,5 +1,29 @@
 package com.tka.Controller;
 
-public class UserController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.tka.Entity.UserEntity;
+import com.tkaService.UserService;
+
+@Controller
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserService userservice;
+	
+	@PostMapping("/resister")
+	public  ResponseEntity<UserEntity> resisterUser(@RequestBody  UserEntity user){
+		UserEntity createUser =userservice.resisterUser(user); 
+		return ResponseEntity.ok(createUser);
+				
+	}
+ 
 }
--- src/main/java/com/tkaService/UserService.java
+++ src/main/java/com/tkaService/UserService.java
@@ -1,5 +1,18 @@
 package com.tkaService;
 
-public class UserService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.tka.Entity.UserEntity;
+import com.tka.Reposiory.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userrepository;
+
+	public UserEntity resisterUser(UserEntity user) {
+		return userrepository.save(user);
+	}
+	
 }
