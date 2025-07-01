68f764915da5afb44ef5685555f5c8ab621bfd0e
Bhargav <vyasb1503@gmail.com>
2025-07-01 22:50:33 +0530

    this is the implementation of the autowire anonation and start for the  user  resistory

--- src/main/java/Service/UserService.java
+++ src/main/java/Service/UserService.java
@@ -1,5 +1,12 @@
 package Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import Repository.UserRepository;
+
 public class UserService {
 
+	@Autowired
+	private UserRepository userrepository;
+	
 }
