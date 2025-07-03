29908d17dd0485f64a2f2907d0a613a7be364400
Bhargav <vyasb1503@gmail.com>
2025-07-03 22:36:25 +0530

    implementing the DTO

---
+++ src/main/java/com/tka/DTO/UserDto.java
@@ -0,0 +1,11 @@
+package com.tka.DTO;
+
+public class UserDto {
+
+	  private int id;
+	    private String name;
+	    private String email;
+	    private int availability;
+	    private String location;
+	    private List<String> interests;
+}
