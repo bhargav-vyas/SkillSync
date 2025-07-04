87cfc9d1ee06725f5085fbc863d15ca428127695
Bhargav <vyasb1503@gmail.com>
2025-07-04 19:19:20 +0530

    changes in  the user entity

--- src/main/java/com/tka/Entity/UserEntity.java
+++ src/main/java/com/tka/Entity/UserEntity.java
@@ -31,5 +31,20 @@ public class UserEntity {
 	private  List<String> interests;
 	private int availability ;
 	private String location;
+	
+	
+	public UserEntity(int id, String name, String email, String password, List<String> skills, List<String> interests,
+			int availability, String location) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.skills = skills;
+		this.interests = interests;
+		this.availability = availability;
+		this.location = location;
+	}
+	
 }
  
