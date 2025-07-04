2d21cf865a0fdfcc1cb1f6f71c22e7f5e9aad67a
Bhargav <vyasb1503@gmail.com>
2025-07-04 19:49:21 +0530

    this is the  change in the get all api

--- src/main/java/com/tka/DTO/UserDto.java
+++ src/main/java/com/tka/DTO/UserDto.java
@@ -1,14 +1,6 @@
 package com.tka.DTO;
-
-import java.util.List;
-
 public class UserDto {
-
-	  private int id;
-	    private String name;
 	    private String email;
-	    private int availability;
-	    private String location;
-	    private List<String> interests;
+	    private String password;
 	    
 }
